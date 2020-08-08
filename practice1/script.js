10d101ae8b47e6f13a8397698f83687094376323
Emanuel Castillo <ecastillo76@misena.edu.co>
2020-08-09 00:38:43 +0200

    Finally I get some points that I didn´t understand :D

--- practice1/script.js
+++ practice1/script.js
@@ -51,6 +51,35 @@ function crearModelo() {
   return modelo
 }
 
+function convertirDatosATensores(data) {
+  return tf.tidy(() => {
+    tf.util.shuffle(data)
+    const entradas = data.map(d => d.cuartos)
+    const etiquetas = data.map(d => d.precio)
+    const tensorEntradas = tf.tensor2d(entradas, [entradas.length, 1])
+
+    const tensorEtiquetas = tf.tensor2d(etiquetas, [etiquetas.length, 1])
+    
+    const entradasMax = tensorEntradas.max()
+    const entradasMin = tensorEntradas.min()
+    const etiquetasMax = tensorEtiquetas.max()
+    const etiquetasMin = tensorEtiquetas.min()
+
+    // (dato-min)/(max-min)
+    const entradasNormalizadas = tensorEntradas.sub(entradasMin).div(entradasMax.sub(entradasMin))
+    const etiquetasNormalizadas = tensorEtiquetas.sub(etiquetasMin).div(etiquetasMax.sub(etiquetasMin))
+
+    return {
+      entradas: entradasNormalizadas,
+      etiquetas: etiquetasNormalizadas,
+      entradasMax,
+      entradasMin,
+      etiquetasMax,
+      etiquetasMin,
+    }
+  })
+}
+
 async function run() {
   //body
   const data = await getData()
